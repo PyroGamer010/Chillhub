ad748178ebceb80c8ca0ceb31ed8c599cf0e4598
ThaOniFans <valentinoandric200425@gmail.com>
2023-02-17 23:04:52 +0100

    taskbar design beetje af

--- client/components/components/taskbar/taskbar.tsx
+++ client/components/components/taskbar/taskbar.tsx
@@ -1,8 +1,13 @@
 import styles from './taskbar.module.scss'
 
 import { BsFillPersonFill } from 'react-icons/bs'
+import { MdFavorite } from 'react-icons/md'
+import { ImFire } from 'react-icons/im'
+import { AiFillStar } from 'react-icons/ai'
+import { FaSearch } from "react-icons/fa"
 
 import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 const Taskbar = () => {
     const { data: session, status } = useSession()
@@ -12,7 +17,82 @@ const Taskbar = () => {
             <div className={styles.taskbar}>
                 <div className={styles.flex}>
                     <div className={styles.pinned}>
-                        
+                        <p>Pinned</p>                   
+                        <div className={styles.pinnedItem}>
+                            <BsFillPersonFill />
+                            <div className={styles.text}>
+                                {session?.user?.name} 
+                                <div>
+                                    genre
+                                </div>
+                            </div>
+                            <div className={styles.viewer}>
+                                <BsFillPersonFill />
+                                <div>
+                                    999
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={styles.pinnedItem}>
+                            <BsFillPersonFill />
+                            <div className={styles.text}>
+                                {session?.user?.name} 
+                                <div>
+                                    genre
+                                </div>
+                            </div>
+                            <div className={styles.viewer}>
+                                <BsFillPersonFill />
+                                <div>
+                                    999
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={styles.pinnedItem}>
+                            <BsFillPersonFill />
+                            <div className={styles.text}>
+                                {session?.user?.name} 
+                                <div>
+                                    genre
+                                </div>
+                            </div>
+                            <div className={styles.viewer}>
+                                <BsFillPersonFill />
+                                <div>
+                                    999
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.boxMiddle}>
+                        <div className={styles.boxCenter}>
+                            <div className={styles.boxItem}>
+                                <AiFillStar />
+                                <Link href="#" className={styles.link}>Favorites</Link>
+                            </div>
+
+                            <div className={styles.boxItem}>
+                                <MdFavorite />
+                                <Link href="#" className={styles.link}>Following</Link>
+                            </div>
+
+                            <div className={styles.boxItem}>
+                                <ImFire />
+                                <Link href="#" className={styles.link}>Top viewers</Link>
+                            </div>
+
+                            <div className={styles.boxItem}>
+                                <FaSearch />
+                                <Link href="#" className={styles.link}>Discover</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.pinned}>
+                        <p>Recommended</p>                        
                         <div className={styles.pinnedItem}>
                             <BsFillPersonFill />
                             <div className={styles.text}>
